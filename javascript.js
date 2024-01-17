106468276e8dcd1c94a5313c33d8d1fba6d9565c
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-17 13:29:21 -0300

    Add divSquare width and height calculations

--- javascript.js
+++ javascript.js
@@ -1,19 +1,29 @@
-function divCreator(xSize,ySize) {
-    for (let i = 1; i <= xSize; i++) {
-        for (let j = 1; j <= ySize; j++) {
+function divCreator(xDivs,yDivs) {
+    for (let i = 1; i <= xDivs; i++) {
+        for (let j = 1; j <= yDivs; j++) {
             const container = document.querySelector('.container');
             const divSquare = document.createElement('div');
             divSquare.classList.add('squareDiv');
-            if (i < 10 && j < 10) {divSquare.classList.add(`x0${i}y0${j}`)}
-            else if (i < 10 && j >= 10) {divSquare.classList.add(`x0${i}y${j}`)}
-            else if (i >= 10 && j < 10) {divSquare.classList.add(`x${i}y0${j}`)}
-            else {divSquare.classList.add(`x${i}y${j}`)};
+            if (i < 10 && j < 10) {divSquare.classList.add(`x0${i}`);divSquare.classList.add(`y0${j}`)}
+            else if (i < 10 && j >= 10) {divSquare.classList.add(`x0${i}`);divSquare.classList.add(`y${j}`)}
+            else if (i >= 10 && j < 10) {divSquare.classList.add(`x${i}`);divSquare.classList.add(`y0${j}`)}
+            else {divSquare.classList.add(`x${i}`);divSquare.classList.add(`y${j}`)};
             container.appendChild(divSquare);
         }
     }
 };
 
-let xSize = 16;
-let ySize = 16;
+let xDivs = 16;
+let yDivs = 16;
 
-divCreator(xSize,ySize);
+let xSize = Math.floor(900 / xDivs);
+let ySize = Math.floor(900 / yDivs);
+
+divCreator(xDivs,yDivs);
+
+let root = document.querySelector(':root');
+let rootStyles = getComputedStyle(root);
+let squareDivWidth = rootStyles.getPropertyValue('--squareDivWidth');
+let squareDivHeight = rootStyles.getPropertyValue('--squareDivHeight');
+root.style.setProperty('--squareDivWidth',`${xSize}px`);
+root.style.setProperty('--squareDivHeight',`${ySize}px`);
