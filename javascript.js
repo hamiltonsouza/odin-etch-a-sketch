5bd7efc10881868c6cc17fec6a8c057a48243a5c
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-16 22:13:45 -0300

    Revert "Add y on div creator"
    This reverts commit 8aefcd7488aa3a795fb90cc13572d2684bcf98c6.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,13 @@
-function divCreator(xSize,ySize) {
+function divCreator(xSize) {
     for (let i = 1; i <= xSize; i++) {
-        for (let j = 1; j <= ySize; j++) {
-            const container = document.querySelector('.container');
-            const divRow = document.createElement('div');
-            if (i < 10 && j < 10) {divRow.classList.add(`x0${i}y0${j}`)}
-            else if (i < 10 && j >= 10) {divRow.classList.add(`x0${i}y${j}`)}
-            else if (i >= 10 && j < 10) {divRow.classList.add(`x${i}y0${j}`)}
-            else {divRow.classList.add(`x${i}y${j}`)}
-            container.appendChild(divRow);
-        }
+        const container = document.querySelector('.container');
+        const divRow = document.createElement('div');
+        if (i < 10){divRow.classList.add(`x0${i}`)}
+        else {divRow.classList.add(`x${i}`)}
+        container.appendChild(divRow);
     }
 };
 
 let xSize = 16;
-let ySize = 16;
 
-divCreator(xSize,ySize);
+divCreator(xSize);
