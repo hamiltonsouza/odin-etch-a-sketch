72188c0dfd4995c0bb3ed2841a54d7287fac497b
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-17 12:47:16 -0300

    Add css tests and tweaks const names

--- javascript.js
+++ javascript.js
@@ -2,12 +2,13 @@ function divCreator(xSize,ySize) {
     for (let i = 1; i <= xSize; i++) {
         for (let j = 1; j <= ySize; j++) {
             const container = document.querySelector('.container');
-            const divRow = document.createElement('div');
-            if (i < 10 && j < 10) {divRow.classList.add(`x0${i}y0${j}`)}
-            else if (i < 10 && j >= 10) {divRow.classList.add(`x0${i}y${j}`)}
-            else if (i >= 10 && j < 10) {divRow.classList.add(`x${i}y0${j}`)}
-            else {divRow.classList.add(`x${i}y${j}`)}
-            container.appendChild(divRow);
+            const divSquare = document.createElement('div');
+            divSquare.classList.add('squareDiv');
+            if (i < 10 && j < 10) {divSquare.classList.add(`x0${i}y0${j}`)}
+            else if (i < 10 && j >= 10) {divSquare.classList.add(`x0${i}y${j}`)}
+            else if (i >= 10 && j < 10) {divSquare.classList.add(`x${i}y0${j}`)}
+            else {divSquare.classList.add(`x${i}y${j}`)};
+            container.appendChild(divSquare);
         }
     }
 };
